d7679a3c1f3409974ce5bc8db6f6c7ef202960c3
Dhruva Pethe <stringtheoryiscorrect@gmail.com>
2026-08-12 20:20:35 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+def introduce():
+    name = "Aldebaran"
+    field = "Artificial Intelligence & Machine Learning"
+
+    print(f"Hello! My name is {name}.")
+    print(f"I am studying {field}.")
+    print("I am interested in programming, AI/ML, and cybersecurity.")
+
+
+if __name__ == "__main__":
+    introduce()
